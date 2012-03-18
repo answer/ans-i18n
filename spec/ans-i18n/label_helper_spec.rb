f527f654184917fe508b7795d29130731ebb1095
sakai shunsuke <sakai@ans-web.co.jp>
2012-03-18 19:23:08 +0900

    変換メソッドの追加

--- spec/ans-i18n/label_helper_spec.rb
+++ spec/ans-i18n/label_helper_spec.rb
@@ -45,5 +45,57 @@ module Ans::I18n
       end
     end
 
+    describe "#t_message" do
+      before do
+        stub(::I18n).t
+
+        controller = Object.new
+        stub(controller).controller_name { "articles" }
+        stub(@helper).controller { controller }
+
+        @attr = :attr
+        @helper.t_message @attr
+      end
+
+      it "は、 messages からメッセージを取得する" do
+        ::I18n.should have_received.t("messages.article.#{@attr}")
+      end
+    end
+
+    describe "#t_message(key,replaces)" do
+      before do
+        @attr = :attr
+
+        stub(::I18n).t("messages.article.#{@attr}"){@message}
+
+        controller = Object.new
+        stub(controller).controller_name { "articles" }
+        stub(@helper).controller { controller }
+      end
+
+      it "は、 messages からメッセージを取得し、置換して返す" do
+        @message = ":key:のメッセージ"
+        @helper.t_message(@attr, key: "value").should == "valueのメッセージ"
+      end
+      it "は、正規表現の特殊文字をキーにしたハッシュでも正しく置換して返す" do
+        @message = ":[name]:のメッセージ"
+        @helper.t_message(@attr, "[name]" => "value").should == "valueのメッセージ"
+      end
+    end
+
+    describe "#t_message_model" do
+      before do
+        stub(::I18n).t
+
+        @model = :model
+        @attr = :attr
+        @helper.t_message_model @model, @attr
+      end
+
+      it "は、 messages からラベルを取得する" do
+        ::I18n.should have_received.t("messages.#{@model}.#{@attr}")
+      end
+    end
+
   end
 end
